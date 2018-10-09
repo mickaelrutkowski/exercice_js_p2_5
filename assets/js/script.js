725de8fe5982d14c806ab5b815201fc80894384f
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-10-09 22:36:23 +0200

    exercice p2 5

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function image () {
+document.getElementById('image1').src = "assets/image/image1_2.jpg";
+document.getElementById('image2').src = "assets/image/image2_2.jpg";
+document.getElementById('image3').src = "assets/image/image3_2.jpg";
+document.getElementById('image4').src = "assets/image/image4_2.jpg";
+document.getElementById('image5').src = "assets/image/image5_2.jpg";
+}
+//donner une action a une image
+//imagae1_2 photo suivante
